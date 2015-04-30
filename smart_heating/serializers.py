50b27f60e1fbe0feca3bdae9e636d9086029bec7
Michael Spiegel <spiagl.m@gmail.com>
2015-04-30 20:35:37 +0200

    Display more resource fields in API

--- smart_heating/serializers.py
+++ smart_heating/serializers.py
@@ -6,6 +6,8 @@ from smart_heating.models import *
 
 class ResidenceSerializer(serializers.HyperlinkedModelSerializer):
     rooms = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    # TODO reference rooms per URL using a custom Field
+    # http://www.django-rest-framework.org/api-guide/relations/#advanced-hyperlinked-fields
     # rooms = serializers.HyperlinkedRelatedField(many=True, view_name='room-detail',
     #                                             lookup_url_kwarg='residence', read_only=True)
     room_base_url = serializers.HyperlinkedIdentityField(view_name='room-list', lookup_url_kwarg='residence_pk')
@@ -17,17 +19,20 @@ class ResidenceSerializer(serializers.HyperlinkedModelSerializer):
 
 class RoomSerializer(serializers.HyperlinkedModelSerializer):
     url = relations.HierarchicalHyperlinkedIdentityField(view_name='room-detail', read_only=True)
+    thermostats = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Room
-        fields = ('id', 'url', 'name', 'residence')
+        fields = ('id', 'url', 'name', 'residence', 'thermostats')
 
 
 # TODO use a HyperlinkedModelSerializer
 class ThermostatSerializer(serializers.ModelSerializer):
+    temperatures = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Thermostat
-        fields = ('rfid', 'room')
+        fields = ('rfid', 'room', 'temperatures')
 
 
 # TODO use a HyperlinkedModelSerializer
@@ -35,4 +40,4 @@ class TemperatureSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Temperature
-        fields = ('datetime', 'value')
+        fields = ('datetime', 'value', 'thermostat')
