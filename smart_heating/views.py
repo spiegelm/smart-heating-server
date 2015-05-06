ebc05cea416fa159df8141fc87ef787457afd00b
Michael Spiegel <spiagl.m@gmail.com>
2015-05-07 01:51:30 +0200

    Simplified ViewSets

--- smart_heating/views.py
+++ smart_heating/views.py
@@ -1,7 +1,6 @@
 # Create your views here.
 
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponseBadRequest
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -43,28 +42,11 @@ class ThermostatViewSet(viewsets.ModelViewSet):
     queryset = Thermostat.objects.all()
     serializer_class = ThermostatSerializer
 
-    def get_queryset(self, residence_pk, room_pk):
-        # check residence and room
+    def get_queryset(self):
+        residence_pk = self.kwargs.get('residence_pk')
+        room_pk = self.kwargs.get('room_pk')
         get_object_or_404(Room.objects.all(), residence=residence_pk, pk=room_pk)
-        return super(ThermostatViewSet, self).get_queryset()
-
-    def list(self, request, residence_pk, room_pk):
-        queryset = self.get_queryset(residence_pk, room_pk)
-        list = queryset.all().filter(room=room_pk)
-        serializer = ThermostatSerializer(list, many=True, context={'request': request})
-        return Response(serializer.data)
-
-    def retrieve(self, request, residence_pk, room_pk, pk):
-        queryset = self.get_queryset(residence_pk, room_pk)
-        # # check residence and room
-        # get_object_or_404(Room.objects.all(), residence=residence, pk=room)
-        thermostat = get_object_or_404(queryset, pk=pk, room=room_pk)
-        serializer = ThermostatSerializer(thermostat, context={'request': request})
-        return Response(serializer.data)
-
-
-from rest_framework import status
-import django
+        return Thermostat.objects.filter(room=room_pk)
 
 
 class TemperatureViewSet(viewsets.ModelViewSet):
@@ -72,32 +54,11 @@ class TemperatureViewSet(viewsets.ModelViewSet):
     queryset = Temperature.objects.all()
     serializer_class = TemperatureSerializer
 
-    def get_queryset(self, residence_pk, room_pk, thermostat_pk):
-        # check residence, room and thermostat
+    def get_queryset(self):
+        residence_pk = self.kwargs.get('residence_pk')
+        room_pk = self.kwargs.get('room_pk')
+        thermostat_pk = self.kwargs.get('thermostat_pk')
+        # check residence, room and thermostat in hierarchy
         get_object_or_404(Room.objects.all(), residence=residence_pk, pk=room_pk)
         get_object_or_404(Thermostat.objects.all(), room=room_pk, pk=thermostat_pk)
-        return super(TemperatureViewSet, self).get_queryset()
-
-    def list(self, request, residence_pk, room_pk, thermostat_pk):
-        queryset = self.get_queryset(residence_pk, room_pk, thermostat_pk)
-        list = queryset.all().filter(thermostat=thermostat_pk)
-        serializer = TemperatureSerializer(list, many=True, context={'request': request})
-        return Response(serializer.data)
-
-    def retrieve(self, request, residence_pk, room_pk, thermostat_pk, pk):
-        queryset = self.get_queryset(residence_pk, room_pk, thermostat_pk)
-        temperature = get_object_or_404(queryset, pk=pk, thermostat=thermostat_pk)
-        serializer = TemperatureSerializer(temperature, context={'request': request})
-        return Response(serializer.data)
-
-    # TODO make create (POST) working
-    # def create(self, request, *args, **kwargs):
-    #     data = request.data
-    #     data.update(kwargs)
-    #     print(data)
-    #
-    #     serializer = TemperatureSerializer(context={'request': request}, data=data)
-    #     serializer.is_valid(raise_exception=True)
-    #     self.perform_create(serializer)
-    #     headers = self.get_success_headers(serializer.data)
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Temperature.objects.filter(thermostat=thermostat_pk)
