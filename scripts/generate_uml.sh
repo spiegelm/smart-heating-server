770e2e27c0fdf1a79e2f50c798a3874452560cd8
Michael Spiegel <spiagl.m@gmail.com>
2015-06-22 05:03:19 +0200

    Exclude Model class from class diagram

--- scripts/generate_uml.sh
+++ scripts/generate_uml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e # Exit script on error
 
-./manage.py graph_models smart_heating > uml_class_diagram.dot
+../manage.py graph_models smart_heating -X Model > uml_class_diagram.dot
 dot -Tpng uml_class_diagram.dot -o uml_class_diagram.png
 # gnome-open uml_class_diagram.png
