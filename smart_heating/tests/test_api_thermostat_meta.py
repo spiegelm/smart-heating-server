70c927aa3d75f1fc61cc0ed31e0697dcb5e981f3
Michael Spiegel <spiagl.m@gmail.com>
2015-11-13 02:44:53 +0100

    Add test. Refactoring.

--- smart_heating/tests/test_api_thermostat_meta.py
+++ smart_heating/tests/test_api_thermostat_meta.py
@@ -31,8 +31,10 @@ class ViewThermostatMetaEntryTestCase(APITestCase):
     def test_meta_entries_collection_consists_of_representations(self):
         date0 = datetime.datetime(2015, 5, 13, 7, 0, 0, 0, timezone.get_current_timezone())
         date1 = datetime.datetime(2015, 5, 13, 8, 0, 0, 0, timezone.get_current_timezone())
-        meta0 = models.ThermostatMetaEntry.objects.create(thermostat=self.thermostat, datetime=date0, rssi=-30, uptime=13, battery=3300)
-        meta1 = models.ThermostatMetaEntry.objects.create(thermostat=self.thermostat, datetime=date1, rssi=-40, uptime=50, battery=3400)
+        meta0 = models.ThermostatMetaEntry.objects.create(thermostat=self.thermostat, datetime=date0,
+                                                          rssi=-30, uptime=13, battery=3300)
+        meta1 = models.ThermostatMetaEntry.objects.create(thermostat=self.thermostat, datetime=date1,
+                                                          rssi=-40, uptime=50, battery=3400)
 
         response_temp0 = self.client.get('/residence/3/room/1/thermostat/5/meta_entry/%s/' % meta0.pk)
         response_temp1 = self.client.get('/residence/3/room/1/thermostat/5/meta_entry/%s/' % meta1.pk)
@@ -46,28 +48,40 @@ class ViewThermostatMetaEntryTestCase(APITestCase):
         self.assertEqual(response.data.get('next_url'), None)
         self.assertEqual(response.data.get('results'), expected_results)
 
+    def test_meta_representation_contains_rssi_uptime_battery(self):
+        date = datetime.datetime(2015, 5, 13, 7, 0, 0, 0, timezone.get_current_timezone())
+        meta_entry = models.ThermostatMetaEntry.objects.create(thermostat=self.thermostat, datetime=date,
+                                                               rssi=-30, uptime=13, battery=3300)
+
+        response = self.client.get('/residence/3/room/1/thermostat/5/meta_entry/%s/' % meta_entry.pk)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('datetime'), '2015-05-13T07:00:00Z')
+        self.assertEqual(response.data.get('rssi'), -30)
+        self.assertEqual(response.data.get('uptime'), 13)
+        self.assertEqual(response.data.get('battery'), 3300)
+
     def test_reverse_url(self):
         date0 = datetime.datetime(2015, 5, 13, 7, 0, 0, 0, timezone.get_current_timezone())
-        meta0 = models.ThermostatMetaEntry.objects.create(thermostat=self.thermostat, datetime=date0, rssi=-30, uptime=13, battery=3300)
+        meta0 = models.ThermostatMetaEntry.objects.create(thermostat=self.thermostat, datetime=date0,
+                                                          rssi=-30, uptime=13, battery=3300)
 
         url = reverse.reverse('thermostatmetaentry-detail', meta0.get_recursive_pks())
         self.assertEqual(url, '/residence/3/room/1/thermostat/5/meta_entry/%s/' % meta0.pk)
 
-    # TODO def test_meta_representation_contains_rssi_uptime_battery(self):
-
-    def test_create_meta_entry(self):
+    def test_can_create_meta_entry(self):
         date0 = datetime.datetime(2015, 5, 13, 7, 0, 0, 0, timezone.get_current_timezone())
         data = {'datetime': date0.isoformat(), 'rssi': 30}
         response = self.client.post('/residence/3/room/1/thermostat/5/meta_entry/', data)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
 
-    def test_refuse_update_meta_entry(self):
+    def test_can_update_meta_entry(self):
         date = datetime.datetime(2015, 5, 13, 7, 0, 0, 0, timezone.get_current_timezone())
-        meta_entry = models.ThermostatMetaEntry.objects.create(thermostat=self.thermostat, datetime=date, rssi=-30)
+        meta_entry = models.ThermostatMetaEntry.objects.create(thermostat=self.thermostat, datetime=date,
+                                                               rssi=-30)
 
-        data = {'rssi': -50, 'uptime': 500, 'battery': 5000}
+        data = {'datetime': date.isoformat(), 'rssi': -50, 'uptime': 500, 'battery': 5000}
 
         response = self.client.put('/residence/3/room/1/thermostat/5/meta_entry/%s/' % meta_entry.pk, data)
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
