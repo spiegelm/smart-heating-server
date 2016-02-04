c5685203c29642a427b8b501cac27828fa37b64a
Michael Spiegel <spiagl.m@gmail.com>
2016-02-04 20:38:58 +0100

    Handle errors more scrictly

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,5 +1,7 @@
 . ~/env/bin/activate
-set -v
+set -e # Abort on error
+set -u # Error on uninitialised variable usage
+set -v # Verbose
 fuser -k 8000/tcp
 pip install -r requirements.txt
 ./manage.py migrate
